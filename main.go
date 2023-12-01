b51af126e6bf76e3c4abac9e2f1242fe70ee6145
Lindolfo <lindolfomoizinho1@gmail.com>
2023-11-30 22:32:52 -0300

    create logger and shcema

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/libdolf/gopportunities/router"
+import (
+	"github.com/libdolf/gopportunities/config"
+	"github.com/libdolf/gopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+
+	// Initialize config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config Initialization Error: %v", err)
+		return
+	}
+
+	// Initialize router
 	router.Initialize()
 }
